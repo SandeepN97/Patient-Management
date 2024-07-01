e61ee994ef28e2b1c357852525b2bf91e348beff
Sandeep Nyoupane <65792729+SandeepN97@users.noreply.github.com>
2024-07-01 10:30:12 -0500

    added new endpoint for test.

---
+++ src/main/java/tech/patientmanagement/PatientmanagementApplication.java
@@ -0,0 +1,13 @@
+package tech.patientmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientmanagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/patientmanagement/PatientmanagementApplicationTests.java
@@ -0,0 +1,14 @@
+/*package tech.patientmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatientmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
+*/
